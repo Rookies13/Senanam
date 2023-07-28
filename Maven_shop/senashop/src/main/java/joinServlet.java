b5e14544852ca35a7f850da94a117b4c6e3aff41
Gyu Sik Kim <te04067@naver.com>
2023-07-28 13:56:19 +0900

    회원가입용 서블릿 파일
    약관 동의 페이지용 서블릿 파일 termServlet.java와 회원가입 페이지용 서블릿 파일 joinServlet.java 업로드

---
+++ Maven_shop/senashop/src/main/java/joinServlet.java
@@ -0,0 +1,144 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/register")
+public class joinServlet extends HttpServlet {
+    // Oracle DB 연결 정보
+    private static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static final String DB_USERNAME = "YOUR_ID";
+    private static final String DB_PASSWORD = "YOUR_PW";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        // 사용자가 입력한 회원 정보 받기
+        String ID = request.getParameter("username");
+        String NAME = request.getParameter("name");
+        String PASSWD = request.getParameter("password");
+        String CONFIRM_PASSWD = request.getParameter("confirmPassword");
+        String EMAIL = request.getParameter("email");
+        String MOBILE = request.getParameter("mobile");
+        String ZIPCODE = request.getParameter("zipcode");
+        String ADDRESS1 = request.getParameter("address1");
+        String ADDRESS2 = request.getParameter("address2");
+
+        if (!PASSWD.equals(CONFIRM_PASSWD)) {
+            out.println("<script>");
+            out.println("alert('비밀번호가 일치하지 않습니다. 다시 시도해주세요.');");
+            out.println("window.location.href='join.jsp';");
+            out.println("</script>");
+            return; // 회원가입 거부
+        }
+
+        // 비밀번호 유효성 검사 (최소 8자리 이상, 영문자와 숫자 조합)
+        if (!isValidPassword(PASSWD)) {
+            out.println("<script>");
+            out.println("alert('비밀번호는 최소 8자리 이상이어야 하며, 영문자와 숫자를 포함해야 합니다.');");
+            out.println("window.location.href='join.jsp';");
+            out.println("</script>");
+            return; // 회원가입 거부
+        }
+
+        if (isIdExists(ID)) {
+            out.println("<script>");
+            out.println("alert('이미 사용 중인 ID입니다. 다른 ID를 입력해주세요.');");
+            out.println("window.location.href='join.jsp';");
+            out.println("</script>");
+            return; // 회원가입 거부
+        }
+
+        // DB 연결 및 회원 정보 삽입
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            String sql = "INSERT INTO member (ID, PASSWD, NAME, EMAIL, MOBILE, ZIPCODE, ADDRESS1, ADDRESS2, USER_LEVEL, TERMCHECK) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, ID);
+            pstmt.setString(2, PASSWD);
+            pstmt.setString(3, NAME);
+            pstmt.setString(4, EMAIL);
+            pstmt.setString(5, MOBILE);
+            pstmt.setString(6, ZIPCODE);
+            pstmt.setString(7, ADDRESS1);
+            pstmt.setString(8, ADDRESS2);
+            pstmt.setInt(9, 0);
+            pstmt.setString(10, "T");
+            pstmt.executeUpdate();
+
+            out.println("<script>");
+            out.println("alert('회원 가입이 완료되었습니다!');");
+            out.println("window.location.href='index.jsp';");
+            out.println("</script>");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            out.println("<script>");
+            out.println("alert('회원 가입 중 오류가 발생했습니다.');");
+            out.println("window.location.href='join.jsp';");
+            out.println("</script>");
+        } finally {
+            try {
+                if (pstmt != null)
+                    pstmt.close();
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean isValidPassword(String password) {
+        // 비밀번호 조건을 정규식으로 검사
+        String passwordPattern = "^(?=.*[A-Za-z])(?=.*\\d).{8,}$";
+        return password.matches(passwordPattern);
+    }
+
+    // ID 중복 확인 메서드
+    private boolean isIdExists(String ID) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            String sql = "SELECT COUNT(*) FROM member WHERE ID = ?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, ID);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                int count = rs.getInt(1);
+                return count > 0;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null)
+                    rs.close();
+                if (pstmt != null)
+                    pstmt.close();
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
---
+++ Maven_shop/senashop/src/main/java/termServlet.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/term")
+public class termServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Check if both checkboxes are checked
+        boolean termsAgree = "true".equals(request.getParameter("terms_agree"));
+        boolean privacyAgree = "true".equals(request.getParameter("privacy_agree"));
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        if (termsAgree && privacyAgree) {
+            // Both checkboxes are checked, proceed to join.jsp
+            response.sendRedirect("join.jsp");
+        } else {
+            // One or both checkboxes are not checked, redirect back to the form
+            out.println("<script>");
+            out.println("alert('모든 약관에 동의해주세요!');");
+            out.println("window.location.href='terms.jsp';");
+            out.println("</script>");
+        }
+    }
+}
